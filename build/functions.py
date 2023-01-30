b3564fe0d5c05525059bfb0e6015b1814c292c20
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2023-01-30 14:42:00 +0800

    Add - functions.py

---
+++ build/functions.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+def display(entry_1, entry_2):
+    try:
+        name = entry_1.get()
+        age = entry_2.get()
+        display = f'The name is {name} and you are {age}'
+        return display
+    except Exception as error:
+        print(f'Error: {error}')
+        return str(error)
+
