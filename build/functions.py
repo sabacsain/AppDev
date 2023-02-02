b8d80ed93b171e1b463f4370ceab4ebc474fddb7
Jerzen Espiritu <109596146+jenjeeen@users.noreply.github.com>
2023-02-02 15:13:26 +0800

    Update functions.py

--- build/functions.py
+++ build/functions.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
-import os
 import gui2, gui3, gui4, gui5, gui6, gui7, gui8
+import datetime
 #  ,,, gui9
 
 def display(entry_1, entry_2):
@@ -21,13 +21,14 @@ def callGui2(window, frame):
 def callGui3(window, frame):
     gui3.start(window, frame)
     
-def callGui4(window, frame):
-    gui4.start(window, frame)
+def callGui4(window, frame, phone):
+    gui4.start(window, frame, phone)
 
-def callGui5(window, frame):
-    gui5.start(window, frame)
+def callGui5(window, frame, phone):
+    gui5.start(window, frame, phone)
 
-def callGui6(window, frame):
+def callGui6(window, frame, sleep_value, phone):
+    sleepTracker(sleep_value, phone)
     gui6.start(window, frame)
 
 def callGui7(window, frame):
@@ -103,7 +104,7 @@ def register(window,frame,fname_entry,lname_entry,phone_entry,birthday_entry,pas
                 conn.commit()
                 conn.close()
 
-                callGui4(window,frame)
+                callGui4(window,frame, phone)
 
         except Exception as error:
             messagebox.showerror("Registration Unsuccessful",f'Error: {error}')
@@ -135,7 +136,6 @@ def login(window,frame,phone_entry,password_entry):
             #check if phone number and password are correct (in the database)
             c.execute("SELECT * FROM accounts where (PHONE=? AND PASSWORD=?)",[phone,password])
             account = c.fetchone()
-
             if(account==None):
                 messagebox.showerror("Error", "Login Failed. Invalid phone number/password was entered.")
                 conn.commit()
@@ -145,11 +145,50 @@ def login(window,frame,phone_entry,password_entry):
                 messagebox.showinfo("Login", "Login Successful!")
                 conn.commit()
                 conn.close()
-                callGui4(window,frame)
+                callGui4(window, frame, phone)
     
         except Exception as error:
             messagebox.showerror("Login Unsuccessful",f'Error: {error}')
             return str(error)
 
+def sleepTracker(sleep, phone):
+    current_date = datetime.datetime.now()
+    current_date = current_date.strftime("%Y-%m-%d")
+    sleep_value = sleep.get()
+
+    if len(sleep_value) == 0:
+        messagebox.showerror("Error","Please satisfy all the fields")
+    else:
+        try:
+            conn = sqlite3.connect('sleep_database.db')
+            c = conn.cursor()
+
+            #CREATE TABLE IF DATABASE DOESNT EXIST
+            c.execute("""CREATE TABLE IF NOT EXISTS sleep_tracker (
+                PHONE text,
+                SLEEP FLOAT,
+                DATE text)""")
+            conn.commit()
+            #check if phone number (in the database)
+            c.execute("SELECT * FROM sleep_tracker where (PHONE=? AND DATE=?)",[phone, current_date])
+            user_sleep = c.fetchone()
+
+            if(user_sleep==None):
+                c.execute("INSERT into sleep_tracker VALUES (:PHONE, :SLEEP, :DATE)",
+                    {
+                        "PHONE": phone,
+                        "SLEEP": sleep_value,
+                        "DATE": current_date
+                    }
+                )
+                conn.commit()
+                conn.close()
+            else:
+                c.execute("UPDATE sleep_tracker SET SLEEP = ? WHERE DATE = ? AND PHONE = ?", [sleep_value, current_date, phone])
+                conn.commit()
+                conn.close()
+        except Exception as error:
+            messagebox.showerror("Failed to save sleep value",f'Error: {error}')
+            return str(error)
        
     return
