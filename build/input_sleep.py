eaa328898c0f28bf8447c09366aa813236fc2e6c
Jerzen Espiritu <109596146+jenjeeen@users.noreply.github.com>
2023-02-12 02:06:36 +0800

    Update input_sleep.py

--- build/input_sleep.py
+++ build/input_sleep.py
@@ -11,14 +11,14 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import functions
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/media/sendoff/sendoff HDD/CS/THIRD YEAR/FIRST SEM/AppDev/hannahGCardiB/build/assets/frame6")
+ASSETS_PATH = OUTPUT_PATH / Path(r"frame6")
 
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
 
-def start(window, frame):
+def start(window, frame, phone):
     window = window
     frame = frame
 
@@ -48,7 +48,7 @@ def start(window, frame):
         image=button_image_1,
         borderwidth=0,
         highlightthickness=0,
-        command=lambda: functions.callResult(window, frame),
+        command=lambda: functions.callResult(window, frame, entry_1, phone),
         relief="flat"
     )
     enter_button.place(
