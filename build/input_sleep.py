b27f0c00457d1e3ce66a5e409cc3ab675476351d
Jerzen Espiritu <109596146+jenjeeen@users.noreply.github.com>
2023-02-12 23:07:29 +0800

    Update input_sleep.py

--- build/input_sleep.py
+++ build/input_sleep.py
@@ -11,7 +11,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import functions
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"frame6")
+ASSETS_PATH = OUTPUT_PATH / Path(r"assets/frame6")
 
 
 def relative_to_assets(path: str) -> Path:
