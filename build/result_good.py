be653e01dcd54d19deec451c1580621f7482fa7a
Shan Allen Bacsain <sabacsain@gmail.com>
2023-02-15 14:20:38 +0800

    UPDATE - imported functions

--- build/result_good.py
+++ build/result_good.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # from tkinter import *
 # Explicit imports to satisfy Flake8
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
-
+import functions
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"assets/frame14")
